b56861147ee47fc376e219d0a66f1a9bb2a65b66
vlknvd <116744722+vlknvd@users.noreply.github.com>
2023-09-26 18:17:40 +0300

    Delete src directory

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-import Board from './board';
-import Character from './character';
-import Game from './game';
-
-const board = new Board();
-const character = new Character();
-const game = new Game(board, character);
-
-game.init();
